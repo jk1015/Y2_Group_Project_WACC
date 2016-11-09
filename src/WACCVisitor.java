cc0b7f02ab88e1aedb4022abfcc1ef225c1ba027
jaspreet randhawa <jaspreetsrandhawa@hotmail.com>
2016-11-09 13:48:28 +0000

    added basic overriden methods for WACCVisitor

--- src/WACCVisitor.java
+++ src/WACCVisitor.java
@@ -1,7 +1,274 @@
 package PACKAGE_NAME;
 
+import antlr.WACCParser;
+import antlr.WACCParserBaseVisitor;
+
 /**
  * Created by ad5115 on 08/11/16.
  */
-public class WACCVisitor {
+public class WACCVisitor extends WACCParserBaseVisitor {
+    @Override
+    public Object visitArgList(WACCParser.ArgListContext ctx) {
+        return super.visitArgList(ctx);
+    }
+
+    @Override
+    public Object visitBlockStat(WACCParser.BlockStatContext ctx) {
+        return super.visitBlockStat(ctx);
+    }
+
+    @Override
+    public Object visitArrayLiter(WACCParser.ArrayLiterContext ctx) {
+        return super.visitArrayLiter(ctx);
+    }
+
+    @Override
+    public Object visitArrayElem(WACCParser.ArrayElemContext ctx) {
+        return super.visitArrayElem(ctx);
+    }
+
+    @Override
+    public Object visitAssignRHS(WACCParser.AssignRHSContext ctx) {
+        return super.visitAssignRHS(ctx);
+    }
+
+    @Override
+    public Object visitAssignLHS(WACCParser.AssignLHSContext ctx) {
+        return super.visitAssignLHS(ctx);
+    }
+
+    @Override
+    public Object visitUnaryOper(WACCParser.UnaryOperContext ctx) {
+        return super.visitUnaryOper(ctx);
+    }
+
+    @Override
+    public Object visitCallFunction(WACCParser.CallFunctionContext ctx) {
+        return super.visitCallFunction(ctx);
+    }
+
+    @Override
+    public Object visitPrintlnStat(WACCParser.PrintlnStatContext ctx) {
+        return super.visitPrintlnStat(ctx);
+    }
+
+    @Override
+    public Object visitProgram(WACCParser.ProgramContext ctx) {
+        return super.visitProgram(ctx);
+    }
+
+    @Override
+    public Object visitType(WACCParser.TypeContext ctx) {
+        return super.visitType(ctx);
+    }
+
+    @Override
+    public Object visitSkipStat(WACCParser.SkipStatContext ctx) {
+        return super.visitSkipStat(ctx);
+    }
+
+    @Override
+    public Object visitFunction(WACCParser.FunctionContext ctx) {
+        return super.visitFunction(ctx);
+    }
+
+    @Override
+    public Object visitReturnStat(WACCParser.ReturnStatContext ctx) {
+        return super.visitReturnStat(ctx);
+    }
+
+    @Override
+    public Object visitExpr4(WACCParser.Expr4Context ctx) {
+        return super.visitExpr4(ctx);
+    }
+
+    @Override
+    public Object visitExpr3(WACCParser.Expr3Context ctx) {
+        return super.visitExpr3(ctx);
+    }
+
+    @Override
+    public Object visitExpr2(WACCParser.Expr2Context ctx) {
+        return super.visitExpr2(ctx);
+    }
+
+    @Override
+    public Object visitStringLiter(WACCParser.StringLiterContext ctx) {
+        return super.visitStringLiter(ctx);
+    }
+
+    @Override
+    public Object visitExpr1(WACCParser.Expr1Context ctx) {
+        return super.visitExpr1(ctx);
+    }
+
+    @Override
+    public Object visitBracketsExpr(WACCParser.BracketsExprContext ctx) {
+        return super.visitBracketsExpr(ctx);
+    }
+
+    @Override
+    public Object visitWhileStat(WACCParser.WhileStatContext ctx) {
+        return super.visitWhileStat(ctx);
+    }
+
+    @Override
+    public Object visitIdentifier(WACCParser.IdentifierContext ctx) {
+        return super.visitIdentifier(ctx);
+    }
+
+    @Override
+    public Object visitArrayType(WACCParser.ArrayTypeContext ctx) {
+        return super.visitArrayType(ctx);
+    }
+
+    @Override
+    public Object visitExpr6(WACCParser.Expr6Context ctx) {
+        return super.visitExpr6(ctx);
+    }
+
+    @Override
+    public Object visitInitAssignStat(WACCParser.InitAssignStatContext ctx) {
+        return super.visitInitAssignStat(ctx);
+    }
+
+    @Override
+    public Object visitExpr5(WACCParser.Expr5Context ctx) {
+        return super.visitExpr5(ctx);
+    }
+
+    @Override
+    public Object visitFreeStat(WACCParser.FreeStatContext ctx) {
+        return super.visitFreeStat(ctx);
+    }
+
+    @Override
+    public Object visitPairElemType(WACCParser.PairElemTypeContext ctx) {
+        return super.visitPairElemType(ctx);
+    }
+
+    @Override
+    public Object visitParamList(WACCParser.ParamListContext ctx) {
+        return super.visitParamList(ctx);
+    }
+
+    @Override
+    public Object visitBaseExpr(WACCParser.BaseExprContext ctx) {
+        return super.visitBaseExpr(ctx);
+    }
+
+    @Override
+    public Object visitReadStat(WACCParser.ReadStatContext ctx) {
+        return super.visitReadStat(ctx);
+    }
+
+    @Override
+    public Object visitPrintStat(WACCParser.PrintStatContext ctx) {
+        return super.visitPrintStat(ctx);
+    }
+
+    @Override
+    public Object visitIntLiter(WACCParser.IntLiterContext ctx) {
+        return super.visitIntLiter(ctx);
+    }
+
+    @Override
+    public Object visitBaseType(WACCParser.BaseTypeContext ctx) {
+        return super.visitBaseType(ctx);
+    }
+
+    @Override
+    public Object visitBinaryOper3(WACCParser.BinaryOper3Context ctx) {
+        return super.visitBinaryOper3(ctx);
+    }
+
+    @Override
+    public Object visitBinaryOper4(WACCParser.BinaryOper4Context ctx) {
+        return super.visitBinaryOper4(ctx);
+    }
+
+    @Override
+    public Object visitBinaryOper5(WACCParser.BinaryOper5Context ctx) {
+        return super.visitBinaryOper5(ctx);
+    }
+
+    @Override
+    public Object visitUnaryExpr(WACCParser.UnaryExprContext ctx) {
+        return super.visitUnaryExpr(ctx);
+    }
+
+    @Override
+    public Object visitBinaryOper6(WACCParser.BinaryOper6Context ctx) {
+        return super.visitBinaryOper6(ctx);
+    }
+
+    @Override
+    public Object visitPairLiter(WACCParser.PairLiterContext ctx) {
+        return super.visitPairLiter(ctx);
+    }
+
+    @Override
+    public Object visitParam(WACCParser.ParamContext ctx) {
+        return super.visitParam(ctx);
+    }
+
+    @Override
+    public Object visitBinaryOper1(WACCParser.BinaryOper1Context ctx) {
+        return super.visitBinaryOper1(ctx);
+    }
+
+    @Override
+    public Object visitBinaryOper2(WACCParser.BinaryOper2Context ctx) {
+        return super.visitBinaryOper2(ctx);
+    }
+
+    @Override
+    public Object visitCharLiter(WACCParser.CharLiterContext ctx) {
+        return super.visitCharLiter(ctx);
+    }
+
+    @Override
+    public Object visitExpr(WACCParser.ExprContext ctx) {
+        return super.visitExpr(ctx);
+    }
+
+    @Override
+    public Object visitPairElem(WACCParser.PairElemContext ctx) {
+        return super.visitPairElem(ctx);
+    }
+
+    @Override
+    public Object visitIfStat(WACCParser.IfStatContext ctx) {
+        return super.visitIfStat(ctx);
+    }
+
+    @Override
+    public Object visitNewPair(WACCParser.NewPairContext ctx) {
+        return super.visitNewPair(ctx);
+    }
+
+    @Override
+    public Object visitExitStat(WACCParser.ExitStatContext ctx) {
+        return super.visitExitStat(ctx);
+    }
+
+    @Override
+    public Object visitBoolLiter(WACCParser.BoolLiterContext ctx) {
+        return super.visitBoolLiter(ctx);
+    }
+
+    @Override
+    public Object visitPairType(WACCParser.PairTypeContext ctx) {
+        return super.visitPairType(ctx);
+    }
+
+    @Override
+    public Object visitSeqStat(WACCParser.SeqStatContext ctx) {
+        return super.visitSeqStat(ctx);
+    }
+
+    @Override
+    public Object visitAssignStat(WACCParser.AssignStatContext ctx) {
+        return super.visitAssignStat(ctx);
+    }
 }
